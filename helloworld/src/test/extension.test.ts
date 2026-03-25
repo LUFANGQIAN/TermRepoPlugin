8bc6b8ea35411667cdcf329ce0c6c9dc423c77f2
lufangqian <mr.wang.vn@gmail.com>
2026-03-25 09:38:56 +0800

    chore(init):初始化hello world项目脚手架
    - 添加hello world项目用以学习vscode插件开发
    - 初始化项目文档

---
+++ helloworld/src/test/extension.test.ts
@@ -0,0 +1,18 @@
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+
+// 您可以导入并使用 'vscode' 模块中的所有 API
+// 也可以导入您的扩展程序进行测试
+import * as vscode from 'vscode';
+// import * as myExtension from '../../extension';
+
+suite('Extension Test Suite', () => {
+	vscode.window.showInformationMessage('Start all tests.');
+
+	test('Sample test', () => {
+		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	});
+});
